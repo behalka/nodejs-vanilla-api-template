ab31263a4b9bd75bf70a05afd657e65a5860ee30
behalka <kaja.behalova@strv.com>
2019-12-13 14:52:55 +0100

    Add: operation input validation

--- src/operations/base.js
+++ src/operations/base.js
@@ -1,11 +1,36 @@
+const Ajv = require('ajv')
 const log = require('../utils/logger')
 
-const baseOperation = ({ name }) => operationCallback => async input => {
-  const start = Date.now()
-  log.info({ service: name }, 'started!')
-  const result = await operationCallback(input)
-  log.info({ service: name, timeElapsed: Date.now() - start }, 'ended!')
-  return result
+const baseOperation = ({ name, schema }) => {
+  // fixme: ajv instance should be shared between services?
+  const ajv = new Ajv()
+  // the function signature is correct
+  const validationFunc = schema ? ajv.compile(schema) : () => true
+
+  return operationCallback => async input => {
+    const start = Date.now()
+    // validation
+    const valid = validationFunc(input)
+    if (!valid) {
+      log.warn(
+        { validationErrors: validationFunc.errors, operation: name },
+        'Operation input invalid',
+      )
+      throw new Error('there were validation errors')
+    }
+
+    // todo: sanitize input
+    log.info({ operation: name, input }, 'started!')
+    const result = await operationCallback(input)
+    log.info(
+      {
+        operation: name,
+        timeElapsed: Date.now() - start,
+      },
+      'ended!',
+    )
+    return result
+  }
 }
 
 module.exports = baseOperation
--- src/operations/event/index.js
+++ src/operations/event/index.js
@@ -1,12 +1,13 @@
 const { compose } = require('ramda')
 const base = require('../base')
+const eventEntity = require('../../entities/event')
 const { eventRepo } = require('../../data-access/repos/index')
 const { makeCreateEvent } = require('./create')
 
 // findUser is a compiled operation that can be used from API, worker, CLI...
 const createEvent = compose(
   // operation static metadata, e.g validation schema, execution context..
-  base({ name: 'myFancyCreateEvent' }),
+  base({ name: 'myFancyCreateEvent', schema: eventEntity.inputSchema }),
   // operation "make"
   makeCreateEvent,
 )({ create: eventRepo.create })
